027c11a92e15e55d06898b60b223295b043061ff
LorenDavie <loren.davie@gmail.com>
2024-08-04 14:22:15 -0400

    more debug

--- dd/utils.py
+++ dd/utils.py
@@ -5,18 +5,23 @@ Utils for DD
 import base64
 import hashlib
 import hmac
+import logging
 
 from django.conf import settings
+
+
+log = logging.getLogger(__file__)
 
 
 def verify_shopify_webhook(request):
     """
     Verfies the request as a genuine shopify webhook.
     """
+    log.debug("Debugging verify_shopify_webhook")
     shopify_hmac_header = request.META.get(settings.SHOPIFY_HMAC_HEADER)
-    print(f"hmac header is: {shopify_hmac_header}")
+    log.debug(f"hmac header is: {shopify_hmac_header}")
     encoded_secret = settings.SHOPIFY_API_SECRET.encode("utf-8")
-    print(f"encoded secret is: {encoded_secret}")
+    log.debug(f"encoded secret is: {encoded_secret}")
     digest = hmac.new(
         encoded_secret,
         request.body,
